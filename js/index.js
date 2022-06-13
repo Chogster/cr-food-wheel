5751e1cc97bce906d9de8cdcb287754c74ecd58b
Bartu Bazna <bartubazna@Bartus-Macbook-Pro.local>
2022-06-13 13:14:36 +0300

    Added project files

---
+++ js/index.js
@@ -0,0 +1,110 @@
+let jsonFile = [];
+let theWheel = {};
+const spinBtn = document.getElementById('spin_button');
+
+window.addEventListener('load', () => {
+    fetch('./list.json').then(res => res.json())
+    .then(data => {
+        jsonFile = data.list;
+        console.log(jsonFile);
+        theWheel = new Winwheel({
+            'numSegments'  : jsonFile.length,     // Specify number of segments.
+            'outerRadius'  : 300,   // Set outer radius so wheel fits inside the background.
+            'textFontSize' : 28,    // Set font size as desired.
+            'segments'     :        // Define segments including colour and text.
+            assignSegments(),
+            'animation' :           // Specify the animation to use.
+            {
+                'type'     : 'spinToStop',
+                'duration' : 5,     // Duration in seconds.
+                'spins'    : 8,     // Number of complete spins.
+                'callbackFinished' : alertPrize
+            }
+        });
+    })
+});
+
+const assignSegments = () => {
+    const colors = [
+        {
+            'fillStyle': '#ECC8AF',
+            'textFillStyle': 'black'
+        },
+        {
+            'fillStyle': '#E7AD99',
+            'textFillStyle': 'black'
+        },
+        {
+            'fillStyle': '#CE796B',
+            'textFillStyle': 'white'
+        },
+        {
+            'fillStyle': '#C18C5D',
+            'textFillStyle': 'white'
+        },
+        {
+            'fillStyle': '#495867',
+            'textFillStyle': 'white'
+        },
+    ];
+    const res = jsonFile.map((obj) => {
+        console.log(Math.floor(Math.random()* (colors.length + 1)));
+        return {
+            'text': obj,
+            ...colors[Math.floor(Math.random()* (colors.length + 1))]
+        }
+    });
+    return res;
+}
+
+// Vars used by the code in this page to do power controls.
+let wheelPower    = 0;
+let wheelSpinning = false;
+
+// -------------------------------------------------------
+// Click handler for spin button.
+// -------------------------------------------------------
+function startSpin()
+{
+    // Ensure that spinning can't be clicked again while already running.
+    if (wheelSpinning == false) {
+        // Based on the power level selected adjust the number of spins for the wheel, the more times is has
+        // to rotate with the duration of the animation the quicker the wheel spins.
+        theWheel.animation.spins = 3;
+        spinBtn.setAttribute('disabled', true);
+
+        // Begin the spin animation by calling startAnimation on the wheel object.
+        theWheel.startAnimation();
+
+        // Set to true so that power can't be changed and spin button re-enabled during
+        // the current animation. The user will have to reset before spinning again.
+        wheelSpinning = true;
+    }
+}
+
+// -------------------------------------------------------
+// Function for reset button.
+// -------------------------------------------------------
+function resetWheel()
+{
+    theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
+    theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
+    theWheel.draw();                // Call draw to render changes to the wheel.
+
+    document.getElementById('pw1').className = "";  // Remove all colours from the power level indicators.
+    document.getElementById('pw2').className = "";
+    document.getElementById('pw3').className = "";
+
+    wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
+}
+
+// -------------------------------------------------------
+// Called when the spin animation has finished by the callback feature of the wheel because I specified callback in the parameters
+// note the indicated segment is passed in as a parmeter as 99% of the time you will want to know this to inform the user of their prize.
+// -------------------------------------------------------
+function alertPrize(indicatedSegment)
+{
+    // Do basic alert of the segment text. You would probably want to do something more interesting with this information.
+    spinBtn.removeAttribute('disabled');
+    alert("You have won " + indicatedSegment.text);
+}
