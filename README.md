7ee1ca219c9390cc617e27f5c2dc4d6ae7c902b7
Bartu Bazna <bartu.bazna@columbiaroad.com>
2022-06-20 15:20:54 +0300

    Finalised the initial version, added readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# CR Food Wheel
+This tool is inteded for use by CR employees in Helsinki.
+
+Ever found yourself wondering what you should be eating today? Fret not, because this is exactly the tool for a roadie like you 🍕 
+
+Just spin the wheel, and you too could be able to decide what to eat without putting much effort in to the decision process 🔮
+
+## Something missing? Want to contribute?
+
+Ping me on slack and we could get it sorted out. Alternatively, you could just send a PR 👾
+
+## Libraries used 
+* Winwheel.js
+* TweenMax
+* Underscore.js
+* Alpine.js
+
+## Disclaimers
+
+Disclaimer 1: I take no responsibility in the case of dissatisfaction towards what the wheel picked for you 🍩
+
+Disclaimer 2: The businesses listed here are not affiliated with CR and this tool is not used for commercial purposes
--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 let theWheel = {};
 let globalState = {};
-let excludedTags = [];
+let includedTags = [];
+let isExclusiveTag = false;
 let wheelSpinning = false;
 const spinBtn = document.getElementById('spin_button');
-const resetBtn = document.getElementById('reset_button');
 
 /**
  * Called at init, returns the state object to render stuff on DOM
@@ -11,12 +11,14 @@ const resetBtn = document.getElementById('reset_button');
  */
 const initData = async () => {
     const fileData = await fetch('./list.json').then(res => {return res.json()});
+    const restaurantData = fileData.list;
     const state = {
-        "data": fileData.list,
-        "tags": assignTags(fileData.list),
+        "data": restaurantData,
+        "tags": assignTags(restaurantData),
     };
-    constructWheel(fileData.list);
+    constructWheel(restaurantData);
     globalState = state;
+    includedTags = state.tags;
     return state;
 }
 
@@ -30,13 +32,13 @@ const constructWheel = (segmentData) => {
         'outerRadius'  : 340,   // Set outer radius so wheel fits inside the background.
         'textFontSize' : 26,    // Set font size as desired.
         'segments'     :        // Define segments including colour and text.
-        assignSegments(segmentData),
+        assignSegments(_.shuffle(segmentData)),
         'animation' :           // Specify the animation to use.
         {
             'type'     : 'spinToStop',
             'duration' : 5,     // Duration in seconds.
             'spins'    : 8,     // Number of complete spins.
-            'callbackFinished' : alertPrize
+            'callbackFinished' : alertDecision
         }
     });
     theWheel.draw();
@@ -56,7 +58,7 @@ const assignTags = (fileData) => {
 }
 
 /**
- * Adds/removes the tag to excludedTags variable, manipulates classList
+ * Adds/removes the tag to includedTags variable, manipulates classList
  * @param {HTMLButtonElement} tagElem 
  */
 const selectTag = (tagElem) => {
@@ -64,24 +66,32 @@ const selectTag = (tagElem) => {
     const isSelected = tagElem.classList.contains('selected');
     if (isSelected) {
         tagElem.classList.remove('selected');
-        excludedTags.push(tagData);
+        includedTags = _.without(includedTags, tagData);
     } else {
         tagElem.classList.add('selected');
-        excludedTags = _.without(excludedTags, tagData);
+        includedTags.push(tagData);
     }
-    excludedTags = _.uniq(excludedTags);
+    includedTags = _.uniq(includedTags);
     adjustWheel();
 }
 
 const adjustWheel = () => {
     let items = [];
-    globalState.data.forEach(item => {
-        if(_.intersection(item.tags, excludedTags).length === 0) {
-            items.push(item);
-        }
-    });
+    let excludedTags = _.difference(globalState.tags, includedTags);
+    if (isExclusiveTag) {
+        globalState.data.forEach(item => {
+            if(_.intersection(item.tags, includedTags).length > 0 && _.intersection(item.tags, excludedTags).length === 0) {
+                items.push(item);
+            }
+        });
+    } else {
+        globalState.data.forEach(item => {
+            if(_.intersection(item.tags, includedTags).length > 0) {
+                items.push(item);
+            }
+        });
+    }
     _.uniq(items);
-    console.log(items);
     constructWheel(items);
 }
 
@@ -128,8 +138,17 @@ const assignSegments = (dataList) => {
     return res;
 }
 
-function startSpin()
-{
+/**
+ * Sets the exclusive tag flag
+ * If true, restaurants that contain excluded tags will not be put on the wheel
+ * @param {Boolean} value 
+ */
+const setExclusiveFlag = (value) => {
+    isExclusiveTag = value;
+    adjustWheel();
+}
+
+const startSpin = () => {
     if (wheelSpinning == false) {
         theWheel.animation.spins = 3;
         spinBtn.setAttribute('disabled', true);
@@ -138,19 +157,41 @@ function startSpin()
     }
 }
 
-function resetWheel()
-{
+const resetWheel = () => {
     theWheel.stopAnimation(false);
     theWheel.rotationAngle = 0;
     theWheel.draw();
     wheelSpinning = false;
     spinBtn.removeAttribute('disabled');
-    resetBtn.setAttribute('disabled', true);
+}
+
+const getAlertTemplate = (indicatedSegment) => {
+    const result = _.findWhere(globalState.data, {name: indicatedSegment.text});
+    let website = result.website == null ? null : result.website;
+    let note = result.note == null ? null : result.note;
+    let res = `It looks like you're going to <b>${indicatedSegment.text}</b>!`;
+    if (website) {
+        res += `<br>Website: <a href="${website}" target="_blank">${website}</a>`;
+    }
+    if (note) {
+        res += `<br><b>NOTE: ${note}`;
+    }
+
+    return res;
 }
 
 
-function alertPrize(indicatedSegment)
-{
-    resetBtn.removeAttribute('disabled');
-    alert("You have won " + indicatedSegment.text);
+const alertDecision = (indicatedSegment) => {
+    Swal.fire({
+        title: 'Woohoo!',
+        html: getAlertTemplate(indicatedSegment),
+        iconHtml: '🥳',
+        confirmButtonText: 'Nice',
+        customClass: {
+            icon: 'no-border'
+        },
+        confirmButtonColor: '#495867',
+        isConfirmed: resetWheel()
+      });
+    // alert("You have won " + indicatedSegment.text);
 }
